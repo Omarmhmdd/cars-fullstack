1296838eec0706b6d4131490cdbf006cfc530292
Omarmhmdd <omarsamirmohamad@gmail.com>
2025-11-12 23:52:22 +0200

    checked everything

--- cars-server/controllers/CarController.php
+++ cars-server/controllers/CarController.php
@@ -2,31 +2,82 @@
 require_once(__DIR__ . "/../models/Car.php");
 require_once(__DIR__ . "/../connection/connection.php");
 require_once(__DIR__ . "/../services/ResponseService.php");
-require_once(__DIR__ ."/../services/ValidationServices.php");
+require_once(__DIR__ . "/../services/ValidationServices.php");
 
 
 //This function will return all cars if no  passed id and specific car if id was passed
-class CarController{
-function getCars(){
-    global $connection;
+class CarController
+{
 
-    if(isset($_GET["id"])){
-        $id = $_GET["id"];
-        $car = Car::find($connection, $id);
-    echo ResponseService::response(200, $car->toArray());
-    return;
-    }else{
-        $car=Car::findAll($connection);
-        $allcars=[];
-        foreach($car as $row){
-           $allcars[]=$row->toArray();
+
+    function getCars()
+    {
+        global $connection;
+
+        if (isset($_GET["id"])) {
+            $id = $_GET["id"];
+            $car = Car::find($connection, $id);
+            echo ResponseService::response(200, $car->toArray());
+            return;
+        } else {
+            $car = Car::findAll($connection);
+            $allcars = [];
+            foreach ($car as $row) {
+                $allcars[] = $row->toArray();
+            }
+            echo ResponseService::response(200, $allcars);
+            return;
         }
-    echo ResponseService::response(200,$allcars);
-    return;
     }
 
-    
+    function createNewCar()
+    {
+        global $connection;
+
+
+        $name  = $_GET['name']  ?? '';
+        $color = $_GET['color'] ?? '';
+        $year  = $_GET['year']  ?? '';
+
+        $name  = ValidationServices::testInput($name);
+        $color = ValidationServices::testInput($color);
+        $year  = ValidationServices::testInput($year);
+        ValidationServices::ValidateInputs($name, $color, $year);
+
+        Car::create($connection, ['name' => $name, 'color' => $color, 'year' => $year]);
+        echo ResponseService::response(200, "car created");
+    }
+
+    //createNewCar();
+    function updateCurrentCar()
+    {
+        global $connection;
+        $id = $_GET['id'] ?? '';
+        $name = $_GET['name'] ?? '';
+        $color = $_GET['color'] ?? '';
+        $year = $_GET['year'] ?? '';
+
+        $name  = ValidationServices::testInput($name);
+        $color = ValidationServices::testInput($color);
+        $year  = ValidationServices::testInput($year);
+        ValidationServices::ValidateInputs($name, $color, $year);
+        Car::update($connection, $id, ['name' => $name, 'color' => $color, 'year' => $year]);
+        echo ResponseService::response(200, "car updated");
+    }
+
+    //updateCurrentCar();
+    function deleteCurrentCar()
+    {
+        global $connection;
+        $id = $_GET['id'];
+        echo ResponseService::response(200, "Table Deleted");
+        Car::delete($connection, $id);
+    }
+    //deleteCurrentCar();
+
 }
+
+
 /*
 //I comment the other functions because first I did the assignment in this way by giving the functions the name of db columns
 // then I implemented it as Charbel asks us during the meeting
@@ -66,48 +117,3 @@ function deleteCar(){
 //if the id is set? then we retrieve the specific car 
 // if no ID, then we retrieve all the cars
 */
-
-function createNewCar(){
-    global $connection;
-
-  
-   $name  = $_GET['name']  ?? '';
-    $color = $_GET['color'] ?? '';
-    $year  = $_GET['year']  ?? '';
-     
-     $name  = ValidationServices::testInput($name);
-    $color = ValidationServices::testInput($color);
-    $year  = ValidationServices::testInput($year);
-    ValidationServices::ValidateInputs($name, $color, $year);
-
-    Car::create($connection, ['name' => $name, 'color' => $color, 'year' => $year]);
-    echo ResponseService::response(200,"car created");
-}
-
-//createNewCar();
-  function updateCurrentCar(){
-       global $connection;
-       $id=$_GET['id']??'';
-       $name=$_GET['name'] ??'';
-       $color=$_GET['color'] ?? '';
-       $year=$_GET['year'] ?? '';
-    
-    $name  = ValidationServices::testInput($name);
-    $color = ValidationServices::testInput($color);
-    $year  = ValidationServices::testInput($year);
-    ValidationServices::ValidateInputs($name, $color, $year);
-    Car::update($connection, $id, ['name' => $name,'color' => $color,'year' => $year]);
-         echo ResponseService::response(200,"car updated");
-        }
-
-//updateCurrentCar();
-function deleteCurrentCar(){
-       global $connection;
-       $id=$_GET['id'];
-Car::delete($connection, $id);
-}
-//deleteCurrentCar();
-
-}
-
-?>
